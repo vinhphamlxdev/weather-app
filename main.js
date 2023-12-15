98bd85e51fa417ef29f3645558bbec64c39f87c3
vinhpham <vinhphamdev0406@gmail.com>
2023-12-15 12:45:53 +0700

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const API_KEY = `a139e22324488946b4cfba798c50544d`;
-const BASE_API = `http://api.openweathermap.org`;
+const BASE_API_WEATHER = `http://api.openweathermap.org`;
 const API_PROVINCES = `https://provinces.open-api.vn/api/?depth=2`;
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
@@ -31,7 +31,7 @@ const app = {
   getForecastByCityName: async function () {
     try {
       const response = await axios.get(
-        `${BASE_API}/data/2.5/weather?q=${this.currentCityName}&appid=${API_KEY}`
+        `${BASE_API_WEATHER}/data/2.5/weather?q=${this.currentCityName}&appid=${API_KEY}`
       );
       console.log("forecase:", response.data);
       if (response && response?.data.cod === 200) {
@@ -46,24 +46,83 @@ const app = {
   formatNumber: function (number) {
     return number < 10 ? `0${number}` : `${number}`;
   },
+  convertTemperature: function (kelvin) {
+    return Math.floor(kelvin - 273.15);
+  },
+  getCurrentDate: function () {
+    let currentYearElm = $(".current-year");
+    let currentMonthElm = $(".current-month");
+    let currentDayElm = $(".current-day");
+    const currentDate = new Date();
+    const currentDay = currentDate.getDate();
+    const currentMonth = currentDate.getMonth() + 1;
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    const monthName = monthNames[currentMonth];
+    currentDayElm.textContent = currentDay;
+    currentMonth.textContent = monthName;
+    currentYearElm.textContext = currentDate.getFullYear();
+  },
   renderCurrentForeCast: function () {
     let sunriseElm = $(".sunrise-value");
     let sunsetElm = $(".sunset-value");
+    let windSpeedElm = $(".wind-speed");
+    let humidityElm = $(".forecast-humidity");
+    let tempFeelsLikeElm = $(".temp__feels-like");
+    let temperatureElm = $(".forecase-temperature");
+    let pressureElm = $(".forecast-pressure");
     const {
-      sys: { sunrise, sunset },
+      sys,
+      weather,
+      wind,
+      main: {
+        temp,
+        feels_like,
+        temp_min,
+        temp_max,
+        pressure,
+        humidity,
+        sea_level,
+      },
+      visibility,
+      wind: { speed },
     } = this.currentForecast;
-    console.log(sys);
-    if (sunrise && sunset) {
-      // const sunriseDate = new Date(sunrise * 1000);
-      // const sunsetDate = new Date(sunset * 1000);
-      // const sunriseHours = this.formatNumber(sunriseDate.getHours());
-      // const sunriseMinutes = this.formatNumber(sunriseDate.getMinute());
-      // const sunsetHours = this.formatNumber(sunsetDate.getHours());
-      // const sunsetMinutes = this.formatNumber(sunsetDate.getMinutes());
-      // console.log(sunriseHours);
-      // sunriseElm.innerText = `${sunriseHours} : ${sunriseMinutes}`;
-      // sunsetElm.innerText = `${sunsetHours} : ${sunsetMinutes}`;
-    }
+    //Temperature
+    //convert kelvin to celsius
+    const currentTempCelsius = this.convertTemperature(temp);
+    temperatureElm.textContent = `${currentTempCelsius}`;
+    //sunrise and sunset
+    const sunriseDate = new Date(sys?.sunrise * 1000);
+    const sunsetDate = new Date(sys?.sunset * 1000);
+    const sunriseHours = this.formatNumber(sunriseDate.getHours());
+    const sunriseMinutes = this.formatNumber(sunriseDate.getMinutes());
+    const sunsetHours = this.formatNumber(sunsetDate?.getHours());
+    const sunsetMinutes = this.formatNumber(sunsetDate?.getMinutes());
+    sunriseElm.textContent = `${sunriseHours}:${sunriseMinutes} AM`;
+    sunsetElm.textContent = `${sunsetHours}:${sunsetMinutes} PM`;
+    //feels like
+    const feelsLikeCelsius = this.convertTemperature(feels_like);
+    tempFeelsLikeElm.textContent = `${feelsLikeCelsius} °C`;
+    //wind speed
+    const windSpeedKmPerHour = wind.speed * 3.6;
+    const windSpeed = windSpeedKmPerHour.toFixed(2);
+    windSpeedElm.textContent = `${windSpeed} Km/h`;
+    //Pressure
+    pressureElm.textContent = `${pressure} MB`;
+    //Humidity
+    humidityElm.textContent = `${humidity}%`;
   },
   renderDropdownItem: function () {
     let newAddressArrs = this.addressData.map((address) => {
@@ -76,7 +135,7 @@ const app = {
     });
     let html = newAddressArrs.map((item, index) => {
       return `
-          <div class="dropdown-item cursor-pointer px-3 py-2 flex justify-start items-center">${item.name}</div>
+          <div data-cityname="${item.name}" class="dropdown-item cursor-pointer px-3 py-2 flex justify-start items-center">${item.name}</div>
           `;
     });
     dropdownList.innerHTML = html.join("");
@@ -84,7 +143,7 @@ const app = {
   getCoordinateByCityName: async function () {
     try {
       const response = await axios.get(
-        `${BASE_API}/geo/1.0/direct?q=${this.currentCityName}&appid=${API_KEY}`
+        `${BASE_API_WEATHER}/geo/1.0/direct?q=${this.currentCityName}&appid=${API_KEY}`
       );
       let currentLocation = response?.data[0];
       if (currentLocation && currentLocation?.lon && currentLocation?.lat) {
@@ -98,7 +157,7 @@ const app = {
   getForecastOnWeek: async function () {
     try {
       const response = await axios.get(
-        `${BASE_API}/data/2.5/forecast?q=${this.currentCityName}&cnt=6&appid==${API_KEY}`
+        `${BASE_API_WEATHER}/data/2.5/forecast?q=${this.currentCityName}&cnt=6&appid==${API_KEY}`
       );
       if (response && response?.list.length > 0) {
         this.forecastOnWeek = response.list;
@@ -118,15 +177,15 @@ const app = {
       await _this.getForecastOnWeek();
     };
     dropdownMenu.onclick = function (event) {
-      // if (!_this.isShowDropdown) {
-      //   _this.isShowDropdown = true;
-      //   dropdownList.classList.add("show");
-      // } else {
-      //   _this.isShowDropdown = false;
-      //   dropdownList.classList.remove("show");
-      // }
+      event.stopPropagation();
       _this.isShowDropdown = !_this.isShowDropdown;
       dropdownList.classList.toggle("show", _this.isShowDropdown);
+    };
+    document.onclick = function (event) {
+      const targetElm = event.target;
+      if (!dropdownMenu.contains(targetElm)) {
+        dropdownList.classList.remove("show");
+      }
     };
   },
 
@@ -135,6 +194,7 @@ const app = {
     this.getCoordinateByCityName();
     this.getForecastOnWeek();
     this.getForecastByCityName();
+    this.getCurrentDate();
     this.handleEvent();
   },
 };
