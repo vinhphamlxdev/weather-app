a8b5c5cacc4c00898cc7cb2ae0749b20704b1197
vinhpham <vinhphamdev0406@gmail.com>
2023-12-15 18:08:27 +0700

    update

--- main.js
+++ main.js
@@ -6,11 +6,14 @@ const $$ = document.querySelectorAll.bind(document);
 const dropdownList = $(".dropdown-list");
 const dropdownMenu = $(".header-user__dropdown");
 const currentSelected = $(".current-selected");
+const cloudStatus = $(".cloud-status");
+const loadingElm = $(".loading");
 const app = {
   isShowDropdown: false,
+  isLoading: false,
   currentCityName: "Ho Chi Minh",
   addressData: [],
-  forecastOnWeek: [],
+  forecastOnWeek: {},
   currentForecast: {},
   coordinate: {
     lat: "",
@@ -30,17 +33,23 @@ const app = {
   },
   getForecastByCityName: async function () {
     try {
+      this.isLoading = true;
+      loadingElm.classList.add("show");
       const response = await axios.get(
         `${BASE_API_WEATHER}/data/2.5/weather?q=${this.currentCityName}&appid=${API_KEY}`
       );
-      console.log("forecase:", response.data);
+
       if (response && response?.data.cod === 200) {
         this.currentForecast = response.data;
-        console.log("current forecase:", this.currentForecast);
         this.renderCurrentForeCast();
+        this.isLoading = false;
+        loadingElm.classList.remove("show");
       }
     } catch (error) {
       console.error("Error when fetching data:", error);
+    } finally {
+      this.isLoading = false;
+      loadingElm.classList.remove("show");
     }
   },
   formatNumber: function (number) {
@@ -75,6 +84,56 @@ const app = {
     currentMonth.textContent = monthName;
     currentYearElm.textContext = currentDate.getFullYear();
   },
+  getStatusWeather: function (iconCode) {
+    switch (iconCode) {
+      case "01d":
+        return {
+          icon: "./assets/clear-day.png",
+          desc: "Clear sky during the day. Bright sunlight illuminates the surroundings.",
+        };
+      case "01n":
+        return {
+          icon: "./assets/clear-night.png",
+          desc: "Clear sky at night. Twinkling stars and possibly a moonlit night.",
+        };
+      case "02d":
+      case "02n":
+      case "03d":
+      case "03n":
+      case "04d":
+      case "04n":
+        return {
+          icon: "./assets/clouds.png",
+          desc: "Partly to mostly cloudy sky. Varying degrees of cloud cover.",
+        };
+      case "09d":
+      case "09n":
+      case "10d":
+      case "10n":
+        return {
+          icon: "./assets/rain.png",
+          desc: "Rainy weather. Expect rainfall, ranging from light to heavy.",
+        };
+      case "11d":
+      case "11n":
+        return {
+          icon: "./assets/thunderstorm.png",
+          desc: "Thunderstorm. Stormy weather with thunder and lightning.",
+        };
+      case "13d":
+      case "13n":
+        return {
+          icon: "./assets/snow.png",
+          desc: "Snowfall. Delicate snowflakes covering the landscape.",
+        };
+      case "50d":
+      case "50n":
+        return {
+          icon: "./assets/mist.png",
+          desc: "Misty conditions. Reduced visibility with a damp atmosphere.",
+        };
+    }
+  },
   renderCurrentForeCast: function () {
     let sunriseElm = $(".sunrise-value");
     let sunsetElm = $(".sunset-value");
@@ -83,6 +142,11 @@ const app = {
     let tempFeelsLikeElm = $(".temp__feels-like");
     let temperatureElm = $(".forecase-temperature");
     let pressureElm = $(".forecast-pressure");
+    let weatherIconStatusElm = $(".weather-status");
+    let mainStatusElm = $(".main-status");
+    let weatherShortDescElm = $(".weather__short-description");
+    let weatherLongDescElm = $(".weather__long-description");
+    let realFellIconElm = $(".real-feel-img");
     const {
       sys,
       weather,
@@ -99,6 +163,15 @@ const app = {
       visibility,
       wind: { speed },
     } = this.currentForecast;
+    const { icon, main: mainStatus, description } = weather[0];
+    const statusData = this.getStatusWeather(icon);
+    weatherIconStatusElm.src = statusData.icon;
+    //long description
+    weatherLongDescElm.textContent = statusData.desc;
+    //main status
+    mainStatusElm.textContent = mainStatus;
+    //description
+    weatherShortDescElm.textContent = description;
     //Temperature
     //convert kelvin to celsius
     const currentTempCelsius = this.convertTemperature(temp);
@@ -115,6 +188,11 @@ const app = {
     //feels like
     const feelsLikeCelsius = this.convertTemperature(feels_like);
     tempFeelsLikeElm.textContent = `${feelsLikeCelsius} °C`;
+    if (feelsLikeCelsius >= 28) {
+      realFellIconElm.src = "./assets/hot.png";
+    } else {
+      realFellIconElm.src = "./assets/cold.png";
+    }
     //wind speed
     const windSpeedKmPerHour = wind.speed * 3.6;
     const windSpeed = windSpeedKmPerHour.toFixed(2);
@@ -124,6 +202,7 @@ const app = {
     //Humidity
     humidityElm.textContent = `${humidity}%`;
   },
+
   renderDropdownItem: function () {
     let newAddressArrs = this.addressData.map((address) => {
       return {
@@ -140,28 +219,15 @@ const app = {
     });
     dropdownList.innerHTML = html.join("");
   },
-  getCoordinateByCityName: async function () {
-    try {
-      const response = await axios.get(
-        `${BASE_API_WEATHER}/geo/1.0/direct?q=${this.currentCityName}&appid=${API_KEY}`
-      );
-      let currentLocation = response?.data[0];
-      if (currentLocation && currentLocation?.lon && currentLocation?.lat) {
-        this.coordinate.lat = currentLocation.lat;
-        this.coordinate.lon = currentLocation.lon;
-      }
-    } catch (error) {
-      console.error("Error when fetching data:", error);
-    }
-  },
+
   getForecastOnWeek: async function () {
     try {
       const response = await axios.get(
-        `${BASE_API_WEATHER}/data/2.5/forecast?q=${this.currentCityName}&cnt=6&appid==${API_KEY}`
+        `http://api.openweathermap.org/data/2.5/forecast?q=ho chi minh&cnt=6&appid=${API_KEY}`
       );
-      if (response && response?.list.length > 0) {
-        this.forecastOnWeek = response.list;
-        console.log(this.forecastOnWeek);
+      if (response && response?.status === 200) {
+        this.forecastOnWeek = response.data;
+        console.log("forecastOnWeek:", response);
       }
     } catch (error) {
       console.error("Error when fetching data:", error);
@@ -171,10 +237,9 @@ const app = {
     const _this = this;
     dropdownList.onclick = async function (event) {
       const cityNode = event.target.closest(".dropdown-item");
-      currentCityName = cityNode.textContent;
-      currentSelected.textContent = currentCityName;
-      console.log("currentCity:", currentCityName);
-      await _this.getForecastOnWeek();
+      currentSelected.textContent = cityNode.textContent;
+      _this.currentCityName = cityNode.textContent;
+      await _this.getForecastByCityName();
     };
     dropdownMenu.onclick = function (event) {
       event.stopPropagation();
@@ -191,7 +256,6 @@ const app = {
 
   start: function () {
     this.getAllAddress();
-    this.getCoordinateByCityName();
     this.getForecastOnWeek();
     this.getForecastByCityName();
     this.getCurrentDate();
